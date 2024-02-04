72ea2cd8a41dfff992256d636860e83497d263ef
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-02-04 21:07:15 +0700

    feat: add dynamic data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import data from "./data.js";
 
 import "./index.css";
 
@@ -27,9 +28,14 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Menu Kita</h2>
-      <Food nama="Nasi Goreng" deskripsi="Nasi yang digoreng dengan bumbu rempah khas Indonesia" harga={25000} foto="food/nasi-goreng.jpg" stok={Math.random() >= 0.5 ? true : false} />
+      <ul className="foods">
+        {data.map((food) => (
+          <Food foodObj={food} key={food.nama} />
+        ))}
+      </ul>
+      {/* <Food nama="Nasi Goreng" deskripsi="Nasi yang digoreng dengan bumbu rempah khas Indonesia" harga={25000} foto="food/nasi-goreng.jpg" stok={Math.random() >= 0.5 ? true : false} />
 
-      <Food nama="Sate Ayam" deskripsi="Sate ayam yang ditusuk dan dibakar, disajikan dengan bumbu kacang" harga={15000} foto="food/sate-ayam.jpg" stok={Math.random() >= 0.5 ? true : false} />
+      <Food nama="Sate Ayam" deskripsi="Sate ayam yang ditusuk dan dibakar, disajikan dengan bumbu kacang" harga={15000} foto="food/sate-ayam.jpg" stok={Math.random() >= 0.5 ? true : false} /> */}
     </main>
   );
 }
@@ -53,14 +59,14 @@ function Footer() {
 
 function Food(props) {
   return (
-    <div className="food">
-      <img src={props.foto} alt={props.nama} width={100} height={70} />
+    <li className="food">
+      <img src={props.foodObj.foto} alt={props.foodObj.nama} width={100} height={70} />
       <div>
-        <h3>{props.nama}</h3>
-        <p>{props.deskripsi}</p>
-        <span>{props.harga}</span>
+        <h3>{props.foodObj.nama}</h3>
+        <p>{props.foodObj.deskripsi}</p>
+        <span>{props.foodObj.harga}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
